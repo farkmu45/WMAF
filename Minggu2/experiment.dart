96cc21c0d9928366cf11d724333fb2a36c2e66de
farkmu45 <farukmicros@outlook.com>
2022-03-13 13:46:21 +0700

    Add experiment file for week 2 and 3

---
+++ Minggu2/experiment.dart
@@ -0,0 +1,127 @@
+void main() {
+  // TIPE DATA
+  // var numberOfApples = 2;
+  // int oranges = 3;
+
+  // bool evaluate = numberOfApples > oranges ? true : false;
+
+  // print(evaluate);
+
+  // var speed = 100.0;
+  // speed = 110.0;
+
+  // final acceleration = 9.8;
+
+  // var myAge = 21;
+  // print(myAge);
+
+  // int yourAge = 21;
+  // print(yourAge);
+
+  // var pi = 3.14;
+  // double c = 29993427;
+
+  // dynamic numberOfKittens;
+  // numberOfKittens = 'There are no kittens';
+
+  // numberOfKittens = 8.0;
+
+  // bool areThereKittens = true;
+
+  // const speedOfLight = 29987;
+  // final planet = 'Jupiter';
+
+  // STRING
+  // var firstName = 'Test';
+  // var lastName = 'TEST';
+
+  // var fullName = '$firstName $lastName';
+
+  // print(fullName);
+  // var nameLength = '${fullName.length}';
+
+  // print(nameLength);
+
+  // var quote = 'If you can\'t ';
+  // print(quote);
+
+  // var parsed = int.parse('23');
+  // print(parsed);
+
+  // var emoji = '\u263a';
+  // print(emoji);
+
+  // var text = 'I\'m not creating the universe'
+  //     '\nI\'m not creating a model of the universe'
+  //     "\nwhich may or not be true";
+  // print(text);
+
+  // var emoji = '\u{1f3af}';
+  // print(emoji);
+
+  // NULLABILITY
+  // int age;
+  // double height;
+  // var err;
+
+  // var error = err ?? "No error";
+
+  // err ??= error;
+  // print(error);
+
+  // print(age?.isEven);
+
+  // CONDITIONALS
+  // var animal = 'fox';
+  // if (animal == 'cat' || animal == 'dog') {
+  //   print('Animal is a house pet');
+  // } else {
+  //   print('Animal is not a house pet');
+  // }
+
+  // switch () {
+  //   case :
+
+  //     break;
+  //   default:
+  // }
+
+  // var piVsE = e > pi ? 'e' : 'pi';
+  // print(piVsE);
+
+  // LOOPING
+  // while () {
+
+  // }
+
+  // do {
+
+  // } while ();
+
+  // var count = 0;
+  // while (count < 10) {
+  //   print('Count is ${count}');
+  //   count++;
+  // }
+
+  //   var dCount = 0;
+  //   var roll;
+
+  //   do {
+  //     roll = next(1, 6);
+  //     dCount++;
+  //     print("After $dCount roll(s), roll is $roll");
+  //   } while (roll != 6);
+
+  // for (var i = 0; i < count; i++) {
+
+  // }
+
+  // for (var item in items) {
+
+  // }
+
+  // for (var i = 1; i <= 10; i++) {
+  //   print(i * i);
+  // }
+}
---
+++ Minggu3/experiment.dart
@@ -0,0 +1,46 @@
+// void unnamed(int num) {
+
+// }
+
+// void named({required int num, required int max}){
+
+// }
+
+// int add() => a + b
+
+void main() {
+  // FUNCTION
+  // var multiply = (int a, int b) {
+  //   return a * b;
+  // };
+
+  // Function applyNum(int test) {
+  //   return (int val) => val * test;
+  // }
+
+  // var test = applyNum(3);
+  // print(test(2));
+
+  // OOP
+  // var gotgStar = Actor("Fark", ['dafjlsdaf']);
+  // print(gotgStar.name);
+}
+
+// class Person {
+//   final String name;
+//   final String blood;
+
+//   Person(this.name, this.blood);
+// }
+
+// class Actor extends Person {
+//   final String name;
+//   var filmography = <String>[];
+//   static var test = "test";
+
+//   Actor(this.name, this.filmography) : super(name, 'B');
+
+//   Actor.rey(this.name) : super("Rey", "Fark");
+
+//   set debut(String val) => filmography.insert(0, val);
+// }
